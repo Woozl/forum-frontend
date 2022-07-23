8f2e251a4d433cc9f47cf9f073205896fa50f10b
David Glymph <Woozl@users.noreply.github.com>
2022-07-22 22:29:51 -0400

    create post page

---
+++ src/pages/create-post.tsx
@@ -0,0 +1,50 @@
+import { Flex, Button, Box, Textarea, Heading } from '@chakra-ui/react';
+import { Formik, Form } from 'formik';
+import { withUrqlClient } from 'next-urql';
+import { useRouter } from 'next/router';
+import { InputField } from '../components/InputField';
+import { Wrapper } from '../components/Wrapper';
+import { useCreatePostMutation } from '../generated/graphql';
+import { createUrqlClient } from '../utils/createUrqlClient';
+
+const CreatePost: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  const [, createPost] = useCreatePostMutation();
+  return (
+    <Wrapper variant='small'>
+      <Formik
+        initialValues={{ title: '', text: '' }}
+        onSubmit={async (v) => {
+          await createPost({ input: v });
+          router.push('/');
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Flex flexDir='column'>
+              <Heading size='md'>Create Post</Heading>
+              <InputField name='title' placeholder='Post Title' label='' />
+              <Box mt='4'>
+                <Textarea
+                  name='text'
+                  placeholder='Enter post text here...'
+                  minHeight='60'
+                />
+              </Box>
+              <Button
+                mt='6'
+                type='submit'
+                colorScheme='blue'
+                isLoading={isSubmitting}
+              >
+                Submit Post
+              </Button>
+            </Flex>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(CreatePost);
