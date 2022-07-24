e679f47354d1601bf6d19410e95f79c12d7e0a91
David Glymph <Woozl@users.noreply.github.com>
2022-07-23 23:38:32 -0400

    switch posts to list items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,20 @@ import { withUrqlClient } from 'next-urql';
 import NavBar from '../components/NavBar';
 import { createUrqlClient } from '../utils/createUrqlClient';
 import { usePostsQuery } from '../generated/graphql';
+import { List, ListItem } from '@chakra-ui/react';
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
   return (
     <>
       <NavBar />
-      <div>Hello World</div>
-      {data?.posts.map((post) => {
-        return <div key={post.id}>{post.title}</div>;
-      }) ?? <div>Loading!!!</div>}
+      {data?.posts ? (
+        <List>
+          {data?.posts.map((post) => (
+            <ListItem key={post.id}>{post.title}</ListItem>
+          ))}
+        </List>
+      ) : null}
     </>
   );
 };
