6a7c84813eb023819bdd1f2e442f7bc519747f90
David Glymph <Woozl@users.noreply.github.com>
2022-08-05 12:04:24 -0400

    attach vote mutation to vote section + loadingState to control UI spinner

--- src/components/VoteSection.tsx
+++ src/components/VoteSection.tsx
@@ -1,16 +1,30 @@
 import { TriangleUpIcon, TriangleDownIcon } from '@chakra-ui/icons';
 import { Flex, Text, IconButton } from '@chakra-ui/react';
-import React from 'react';
-import { PostSnippetFragment } from '../generated/graphql';
+import React, { useState } from 'react';
+import { PostSnippetFragment, useVoteMutation } from '../generated/graphql';
 
 interface VoteSectionProps {
   post: PostSnippetFragment;
 }
 
 export const VoteSection: React.FC<VoteSectionProps> = ({ post }) => {
+  const [loadingState, setLoadingState] = useState<
+    'upvote-loading' | 'downvote-loading' | 'not-loading'
+  >('not-loading');
+  const [, vote] = useVoteMutation();
+
   return (
     <Flex direction='column' justifyContent='center' alignItems='center' mr='4'>
       <IconButton
+        onClick={async () => {
+          setLoadingState('upvote-loading');
+          await vote({
+            postId: post.id,
+            value: 1
+          });
+          setLoadingState('not-loading');
+        }}
+        isLoading={loadingState === 'upvote-loading'}
         size='sm'
         aria-label='upvote'
         colorScheme='blue'
@@ -18,6 +32,15 @@ export const VoteSection: React.FC<VoteSectionProps> = ({ post }) => {
       />
       <Text fontSize='xl'>{post.points}</Text>
       <IconButton
+        onClick={async () => {
+          setLoadingState('downvote-loading');
+          await vote({
+            postId: post.id,
+            value: -1
+          });
+          setLoadingState('not-loading');
+        }}
+        isLoading={loadingState === 'downvote-loading'}
         size='sm'
         aria-label='downvote'
         colorScheme='red'
